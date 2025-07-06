1c437db5facd6cc04c1adfd9a6dd7cd88b0efcb7
sintaxis9 <skizosyntax@duck.com>
2025-07-06 07:39:20 -0400

    estabilizar un cambio en database

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 client = MongoClient(MONGO_URI)
 
 db = client["focusteam"]
